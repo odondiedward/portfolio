5b38a7afd7dc2033643004c4ec03034745de009f
odondiedward <odondiedward01@gmail.com>
2026-02-27 01:44:54 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const toggles = document.querySelectorAll(".job-toggle");
-
-toggles.forEach(toggle => {
-  toggle.addEventListener("click", () => {
-    const content = toggle.nextElementSibling;
-    content.style.display =
-      content.style.display === "block" ? "none" : "block";
-  });
-});
